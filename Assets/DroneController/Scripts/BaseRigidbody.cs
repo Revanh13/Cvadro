fdd1af0767794f4ad0133cc8888c44399d98581b
Ivan <vana130102@icloud.com>
2021-04-01 17:01:06 +0500

    Drone maid!

---
+++ Assets/DroneController/Scripts/BaseRigidbody.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Drone
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class BaseRigidbody : MonoBehaviour
+    {
+        [Header("Rigidbody Properties")]
+        [SerializeField] private float weightInKg = 1f;
+
+        protected Rigidbody rb;
+        protected float startDrag;
+        protected float startAngularDrag;
+
+        void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+            if (rb)
+            {
+                rb.mass = weightInKg;
+                startDrag = rb.drag;
+                startAngularDrag = rb.angularDrag;
+            }
+        }
+
+        void FixedUpdate()
+        {
+            if (!rb)
+            {
+                return;
+            }
+
+            HandlePhysics();
+        }
+
+        protected virtual void HandlePhysics() { }
+    }
+}
+
---
+++ Assets/DroneController/Scripts/DroneController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace Drone
+{
+    [RequireComponent(typeof(DroneInputs))]
+    public class DroneController : BaseRigidbody
+    {
+        [Header("Control Properties")]
+        [SerializeField] private float minMaxPitch = 30f;
+        [SerializeField] private float minMaxRoll = 30f;
+        [SerializeField] private float yawPower = 4f;
+        [SerializeField] private float lerpSpeed = 2f;
+
+        private DroneInputs input;
+        private List<IEngine> engines = new List<IEngine>();
+
+        private float finalPitch;
+        private float finalRoll;
+        private float yaw;
+        private float finalYaw;
+
+        void Start()
+        {
+            input = GetComponent<DroneInputs>();
+            engines = GetComponentsInChildren<IEngine>().ToList<IEngine>();
+        }
+
+        protected override void HandlePhysics()
+        {
+            HandleEngines();
+            HandleControls();
+        }
+
+        protected virtual void HandleEngines()
+        {
+            //rb.AddForce(Vector3.up * Physics.gravity.magnitude);
+            foreach(IEngine engine in engines)
+            {
+                engine.UpdateEngine(rb, input);
+            }
+        }
+
+        protected virtual void HandleControls()
+        {
+            float pitch = input.Cyclic.y * minMaxPitch;
+            float roll = -input.Cyclic.x * minMaxRoll;
+            yaw += input.Pedals * yawPower;
+
+            finalPitch = Mathf.Lerp(finalPitch, pitch, Time.deltaTime * lerpSpeed);
+            finalRoll = Mathf.Lerp(finalRoll, roll, Time.deltaTime * lerpSpeed);
+            finalYaw = Mathf.Lerp(finalYaw, yaw, Time.deltaTime * lerpSpeed);
+
+            Quaternion rot = Quaternion.Euler(finalPitch, finalYaw, finalRoll);
+            rb.MoveRotation(rot);
+        }
+    }
+}
